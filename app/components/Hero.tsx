ab0340dadf7f173acf955798e2cc2d0baa17b788
elazzouzimohamed <elazzouzimohamed2018@gmail.com>
2024-09-25 17:54:01 +0100

    teste 2

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -4,6 +4,8 @@ const Hero = () => {
   return (
     <div>
       <h1>hna my hero</h1>
+      <p>Lorem ipsum dolor sit amet
+         consectetur adipisicing elit. Neque beatae sequi consectetur odit placeat aspernatur corrupti est, molestias ad cumque.</p>
     </div>
   )
 }
